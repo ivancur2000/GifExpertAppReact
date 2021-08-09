2ad0c3fd120b0910cfe70ab59a4ba3e75cc94ba5
Ivan Curtiñez <ivancurtinez.charli@gmail.com>
2021-08-09 02:05:09 -0400

    hooks personalizados

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,20 +1,17 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 import GifGridItem from './GifGridItem';
-import { getGifs } from '../helpers/getGifs';
 
 const GifGrid = ({category})=>{
 
-  const [images, setImages] = useState([]);
 
-  useEffect(() =>{
-    getGifs(category)
-    .then(setImages);
-  },[category]);
+  const {data:images,loading} = useFetchGifs(category);
 
   return(
     <>
-      <h3>{category}</h3>
+      <h3 className='animate__animated animate__flipInX'>{category}</h3>
+      {loading && <p className='animate__animated animate__flash'>Loading...</p>}
       <div className='cardGrid'>
         {
           images.map((img)=>(
